9b17b119d0e477f9b3e3a5caf0fc106578679daa
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-03-24 15:11:08 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+clear
+apt-get install figlet
+apt-get install cowsay fortune-mod -y
+ln -s /usr/games/cowsay /bin
+ln -s /usr/games/fortune /bin
+
+# ==========================================
+#text,A=Merah,B=Hijau,C=Magenta,D=Cyan
+A='\033[1;31m'
+B='\033[1;32m'
+C='\033[1;35m'
+D='\033[1;36m'
+
+#background,A1=Merah,B1=Hijau,C1=Magenta,D1=Cyan
+A1='\033[1;41m'
+B1='\033[1;42m'
+C1='\033[1;45m'
+D1='\033[1;46m'
+#reset
+R='\033[0m'
+# ==========================================
+
+clear
+if [ "${EUID}" -ne 0 ]; then
+clear
+echo -e ""
+echo -e "${D}——————————————————————————————————————————${R}"
+echo -e "${D1}       Proses Semakan Pengguna Root       ${R}"
+echo -e "${D}——————————————————————————————————————————${R}"
+echo -e ""
+sleep 2
+		echo -e "${A} Maaf, anda perlu menjalankan skrip ini sebagai pengguna root${R}"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+clear
+echo -e ""
+echo -e "${D}——————————————————————————————————————————${R}"
+echo -e "${D1}         Proses Semakan Sistem VPS        ${R}"
+echo -e "${D}——————————————————————————————————————————${R}"
+echo -e ""
+sleep 2
+		echo -e "${A} Maaf, sistem operasi jenis OpenVZ tidak support untuk skrip ini${R}"
+		exit 1
+fi
+
+MYIP=$(wget -qO- ipinfo.io/ip);
+
+IZIN=$( curl https://raw.githubusercontent.com/rewasu91/public/main/ipaddress.sh | grep $MYIP )
+
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${D}——————————————————————————————————————————${R}"
+echo -e "${D1}         Proses Semakan IP Address        ${R}"
+echo -e "${D}——————————————————————————————————————————${R}"
+echo -e ""
+sleep 2
+echo -e "${B} Tahniah, IP address anda telah berdaftar! ${R}"
+echo -e "${B} Proses pemasangan skrip vps akan bermula sekarang..${R}"
+sleep 3
+else
+clear
+echo -e "${D}——————————————————————————————————————————${R}"
+echo -e "${D1}         Proses Semakan IP Address        ${R}"
+echo -e "${D}——————————————————————————————————————————${R}"
+echo -e ""
+sleep 2
+echo -e "${A} Maaf. IP address anda tidak berdaftar dengan admin! ${R}"
+echo -e "${A} Sila hubungi admin di telegram KaizenVPN..${R}"
+sleep 3
+clear
+exit 0
+fi
+
+clear
+echo -e ""
+echo -e ""
+cowsay -f ghostbusters "SELAMAT DATANG BOSKU."
+echo -e ""
+figlet -k '            KAIZEN'
+echo -e ""
+echo -e "     —————————————————————————————————————————————————————————"
+echo -e "     *             Autoskrip VPS Premium KaizenVPN           *"
+echo -e "     —————————————————————————————————————————————————————————"
+echo -e "     *                                                       *"
+echo -e "     * Pakej Yang Tersedia                                   *"
+echo -e "     * [A] Servis SSH, Dropbear, OpenVPN, Squid              *"
+echo -e "     * [B] Servis Websocket, Servis OHP                      *"
+echo -e "     * [C] Servis L2TP, Servis SSTP, Servis PPTP             *"
+echo -e "     * [D] Servis Wireguard, Servis Shadowsocks & SSR        *"
+echo -e "     * [E] Servis V2ray Vmess/Vless/Trojan (Tls, None Tls)   *"
+echo -e "     * [F] Servis Xray Vmess/Vless/Trojan (Tls, None Tls,    *"
+echo -e "     *     Kcp, Xtls, Grpc)                                  *"
+echo -e "     * [G] Servis Trojan Go                                  *"
+echo -e "     —————————————————————————————————————————————————————————"
+echo -e "     *                                                       *"
+echo -e "     * Sila pilih pakej servis yang anda ingin pasang:-      *"
+echo -e "     * [1] Pakej 1 - Semua Servis [A,B,C,D,E,F,G]            *"
+echo -e "     * [2] Pakej 2 - Semua Servis [A,B,C,D,E,F,G]            *"
+echo -e "     * [2] Pakej 2 - Core V2ray V1 [A,B,C,D,E]               *"
+echo -e "     * [3] Pakej 3 - Core Xray V1 [A,B,C,D,F]                *"
+echo -e "     * [4] Pakej 4 - Core V2ray + Trojan Go [A,B,C,D,E,G]    *"
+echo -e "     * [5] Pakej 5 - Core Xray + Trojan Go [A,B,C,D,F,G]     *"
+echo -e "     —————————————————————————————————————————————————————————"
+echo -e "     * PROSES PEMASANGAN SKRIP AKAN BERMULA SEBENTAR LAGI..  *"
+echo -e "     *                  TERIMA KASIH BOSKU!                  *"
+echo -e "     —————————————————————————————————————————————————————————"
+echo -e ""
+sleep 8
+clear
