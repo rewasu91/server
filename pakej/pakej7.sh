454d610d211be82e9258a9e7d70b7a5bee3b7b79
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-03-28 12:46:58 +0800

    Update pakej7.sh

--- pakej/pakej7.sh
+++ pakej/pakej7.sh
@@ -154,7 +154,7 @@ echo -e "${D1}        Memasang Servis SSH & OPENVPN        ${R}"
 echo -e "${D}—————————————————————————————————————————————${R}"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/rewasu91/vps/main/install/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/rewasu91/vps/main/install/ssh-vpn7.sh && chmod +x ssh-vpn7.sh && ./ssh-vpn7.sh
 clear
 echo -e ""
 echo -e "${D}—————————————————————————————————————————————————————${R}"
