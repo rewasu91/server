074cd5a8bb0da46704c2998df9f339873f214da3
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-03-28 15:27:05 +0800

    Update menu-port.sh

--- menu/menupakej4/menu-port.sh
+++ menu/menupakej4/menu-port.sh
@@ -52,23 +52,21 @@ echo -e "${B}[02]${R} ► Tukar port Stunnel4"
 echo -e "${B}[03]${R} ► Tukar port Squid"
 echo -e "${B}[04]${R} ► Tukar port SSTP"
 echo -e "${B}[05]${R} ► Tukar port Wireguard"
-echo -e "${B}[06]${R} ► Tukar port V2ray Vless"
-echo -e "${B}[07]${R} ► Tukar port V2ray Vmess"
-echo -e "${B}[08]${R} ► Tukar port Xray Vless"
-echo -e "${B}[09]${R} ► Tukar port Xray Vmess"
-echo -e "${B}[10]${R} ► Tukar port Xray Grpc"
-echo -e "${B}[11]${R} ► Tukar port Trojan"
-echo -e "${B}[12]${R} ► Kembali ke Menu Utama"
-echo -e "${B}[13]${R} ► Keluar"
+echo -e "${B}[06]${R} ► Tukar port Xray Vless"
+echo -e "${B}[07]${R} ► Tukar port Xray Vmess"
+echo -e "${B}[08]${R} ► Tukar port Xray Grpc"
+echo -e "${B}[091]${R} ► Tukar port Trojan"
+echo -e "${B}[10]${R} ► Kembali ke Menu Utama"
+echo -e "${B}[11]${R} ► Keluar"
 echo -e ""
 echo -e "${D}——————————————————————————————————————————————————————————${R}"
-read -p "     ► Sila masukkan nombor pilihan anda [1-13]: " menuport
+read -p "     ► Sila masukkan nombor pilihan anda [1-11]: " menuport
 echo -e ""
 case $menuport in
 1)
 port-ovpn
 ;;
-1)
+2)
 port-stunnel4
 ;;
 3)
@@ -81,24 +79,18 @@ port-sstp
 port-wg
 ;;
 6)
-port-v2rayvless
-;;
-7)
-port-v2rayvmess
-;;
-8)
 port-xrayvless
 ;;
-9)
+7)
 port-xrayvmess
 ;;
-10)
+8)
 port-xraygrpc
 ;;
-11)
+9)
 port-trojan
 ;;
-12)
+10)
 menu
 ;;
 13)
