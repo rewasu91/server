4cb25baed989363f992e2593b62eb3836fc3e8f0
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-03-28 12:19:45 +0800

    Add files via upload

---
+++ info/info-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo -e ""
+echo -e ""
+cowsay -f ghostbusters "SELAMAT DATANG BOSKU."
+echo -e ""
+figlet -k '         KAIZEN'
+echo -e ""
+echo -e "     ${D}—————————————————————————————————————————————————————————${R}"
+echo -e "     ${D1}*             Autoskrip VPS Premium KaizenVPN           *${R}"
+echo -e "     ${D}—————————————————————————————————————————————————————————${R}"
+echo -e "     ${D}*            Dibuat & Diubahsuai oleh KaizenVPN         *${R}"
+echo -e "     ${D}*            Untuk Debian 9 & Debian 10 64 bit          *${R}"
+echo -e "     ${D}*        Untuk Ubuntu 18.04 & Ubuntu 20.04 64 bit       *${R}"
+echo -e "     ${D}*          Untuk VPS sistem jenis KVM dan VMWare        *${R}"
+echo -e "     ${D}*                    Telegram: @KaizenA                 *${R}"
+echo -e "     ${D}—————————————————————————————————————————————————————————${R}"
+echo -e ""
---
+++ info/info-system.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+neofetch --ascii_distro Minix
+cat /root/log-install.txt
